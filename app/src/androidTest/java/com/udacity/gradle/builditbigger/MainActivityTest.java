9a40b6114691fd56ad7adf92841c4633268ad555
Uli Wucherer <u.wucherer@gmail.com>
2016-02-06 18:21:03 +0100

    add instrumentation ui test for MainActivity
    - added espresso and hamcrest
    - create test for button, ad and intent that opens joke to display
    fixes #4

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/MainActivityTest.java
@@ -0,0 +1,54 @@
+package com.udacity.gradle.builditbigger;
+
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import com.udacity.h3u.gradle.androidjokelibrary.JokeActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.BundleMatchers.hasEntry;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtras;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.Matchers.isEmptyOrNullString;
+
+
+/**
+ * Created by Uli Wucherer (u.wucherer@gmail.com) on 05/02/16.
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityTest {
+    @Rule
+    public IntentsTestRule<MainActivity> mActivityRule = new IntentsTestRule<>(
+            MainActivity.class);
+
+    @Test
+    public void buttonHasText() {
+        onView(withId(R.id.buttonJoke)).check(matches(withText(R.string.button_text)));
+    }
+
+    @Test
+    public void adMobIsDisplayed() {
+        onView(withId(R.id.adView)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void startJokeActivityWithNonEmptyStringExtra() {
+        onView(withId(R.id.buttonJoke)).perform(click());
+
+        intended(hasExtras(hasEntry(
+                equalTo(JokeActivity.INTENT_EXTRA_JOKE), not(isEmptyOrNullString()))));
+    }
+}
